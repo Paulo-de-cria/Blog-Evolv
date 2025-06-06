998b17af3e3f51ed411d06030f130f34bd5fa646
Paulo-de-cria <paulohenriquebenetti5@gmail.com>
2025-06-06 01:25:06 -0300

    api consumida com sucesso
    a api foi iserida e consumida com sucesso e alem disso melhorei o design do html e css, para uma melhor visualização e pra ficar mais bonito

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
 angular.module('blog', []);
-angular.module('blog').controller('Rest', function ($scope, $http){
-  $http.get('https://api-fake-blog.onrender.com/postagens').
-    success(function(data) {
-      $scope.publicacoes = data;
-    }
-  );
+
+angular.module('blog').controller('Rest', function ($scope, $http) {
+  // Carregar lista de postagens
+  $http.get('https://api-fake-blog.onrender.com/postagens')
+    .then(function(response) {
+      $scope.publicacoes = response.data;
+    })
+    .catch(function(error) {
+      console.error("Erro ao carregar postagens:", error);
+    });
+
+  // Função para abrir postagem
+  $scope.abrirPostagem = function(id) {
+    $http.get('https://api-fake-blog.onrender.com/postagem/' + id)
+      .then(function(response) {
+        $scope.postagemSelecionada = response.data;
+      })
+      .catch(function(error) {
+        console.error("Erro ao carregar postagem:", error);
+      });
+  };
+
+  // Função para voltar
+  $scope.voltar = function() {
+    $scope.postagemSelecionada = null;
+  };
 });
